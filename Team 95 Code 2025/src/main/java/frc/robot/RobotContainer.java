a91ad1f1272007f200da83a7c23437bf4065ff60
PhilG2561 <philip2561@gmail.com>
2025-01-06 20:29:08 -0500

    Imported to 2025. Not compiling, needs fixing

---
+++ Team 95 Code 2025/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,318 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.Auton;
+import frc.robot.Constants.ClimberConstants;
+import frc.robot.Constants.CommandDebugFlags;
+import frc.robot.Constants.Drivebase;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.Constants.Vision;
+import frc.robot.commands.AutoAmp;
+import frc.robot.commands.AutoShoot;
+import frc.robot.commands.Autos;
+import frc.robot.commands.drivebase.AbsoluteDrive;
+import frc.robot.commands.NoteHandlerCommand;
+import frc.robot.subsystems.Climber;
+//import frc.robot.commands.drivebase.TeleopDrive;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.SwerveBase;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.choreo.lib.Choreo;
+import com.choreo.lib.ChoreoTrajectory;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+//import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  private final Map<String, ChoreoTrajectory> trajMap;
+
+  // The robot's subsystems and commands are defined here...
+  private final Intake intake = new Intake();
+  private final SwerveBase drivebase = new SwerveBase();
+  private final Shooter shooter = new Shooter();
+  private final Climber climber = new Climber();
+  //private final TeleopDrive openRobotRel, closedRobotRel, openFieldRel, closedFieldRel;
+  private final AbsoluteDrive absoluteDrive;
+
+  private final CommandJoystick driveController = new CommandJoystick(OperatorConstants.driveControllerPort);
+  private final CommandJoystick headingController = new CommandJoystick(OperatorConstants.headingControllerPort);
+  private final CommandXboxController operatorController = new CommandXboxController(
+      OperatorConstants.operatorControllerPort);
+  
+  private Command autoCommand;
+  SendableChooser<Command> autoChooser = new SendableChooser<>();
+  SendableChooser<Integer> debugMode = new SendableChooser<>();
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+
+    /*openRobotRel = new TeleopDrive(
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getY()
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getX()
+            : 0,
+        () -> -headingController.getTwist(), () -> false, true);
+
+    closedRobotRel = new TeleopDrive(
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getY()
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getX()
+            : 0,
+        () -> -headingController.getTwist(), () -> false, false);
+
+    openFieldRel = new TeleopDrive(
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
+            ? driveController.getY()
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
+            ? driveController.getX()
+            : 0,
+        () -> -headingController.getTwist(), () -> true, true);
+
+    closedFieldRel = new TeleopDrive(
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getY()
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getX()
+            : 0,
+        () -> -headingController.getTwist(), () -> true, false);*/
+
+    absoluteDrive = new AbsoluteDrive(
+        drivebase,
+        () -> (Math.abs(driveController.getY()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getY()
+            : 0,
+        () -> (Math.abs(driveController.getX()) > OperatorConstants.joystickDeadband)
+            ? -driveController.getX()
+            : 0,
+        () -> -headingController.getX(),
+        () -> -headingController.getY(),
+        false,
+        () -> driveController.getHID().getRawButton(3));
+
+    NoteHandlerCommand noteManager = new NoteHandlerCommand(
+        shooter,
+        intake,
+        () -> (operatorController.getLeftTriggerAxis() - operatorController.getRightTriggerAxis()),
+        () -> operatorController.getHID().getYButton(),
+        () -> operatorController.getHID().getLeftBumper(),
+        () -> operatorController.getHID().getRightBumper(),
+        () -> headingController.getHID().getRawButton(2),
+        () -> operatorController.getHID().getBButton(),
+        () -> operatorController.getHID().getAButton(),
+        () -> operatorController.getHID().getXButton(),
+        () -> driveController.getHID().getRawButton(2),
+        () -> driveController.getHID().getRawButton(4));
+
+    shooter.setDefaultCommand(noteManager);
+
+    drivebase.setDefaultCommand(absoluteDrive);
+    // Configure the trigger bindings
+    configureBindings();
+
+    SmartDashboard.putString("Selected Auto:", "NONE!!!");
+
+    trajMap = loadTrajectories();
+
+    Command oneNoteAuto = Autos.shootPreload(drivebase);
+    Command centerAuto = Autos.twoNoteCenter(drivebase, intake);
+    Command centerAmpAuto = Autos.threeNoteCenterAmp(drivebase, intake, trajMap);
+    Command fourNoteAuto = Autos.fourNearNotes(drivebase, trajMap);
+    Command podiumAuto = Autos.twoNotePodium(drivebase, intake);
+    Command ampAuto = Autos.twoNoteAmp(drivebase, intake);
+    Command ampCenterAuto = Autos.threeNoteAmpCenter(drivebase, intake, trajMap);
+    Command centerPodium = Autos.threeNoteCenterPodium(drivebase, intake, trajMap);
+    Command centerLineDisruptor = Autos.midlineDisruptor(drivebase, trajMap);
+    Command source3 = Autos.sourceThree(drivebase, trajMap);
+    Command amp3 = Autos.ampMidLineThree(drivebase, trajMap);
+    autoChooser.addOption(oneNoteAuto.getName(), oneNoteAuto);
+    autoChooser.addOption(centerAuto.getName(), centerAuto);
+    autoChooser.addOption(centerAmpAuto.getName(), centerAmpAuto);
+    autoChooser.addOption(podiumAuto.getName(), podiumAuto);
+    autoChooser.addOption(ampAuto.getName(), ampAuto);
+    autoChooser.addOption(ampCenterAuto.getName(), ampCenterAuto);
+    autoChooser.addOption(centerPodium.getName(), centerPodium);
+    autoChooser.addOption(centerLineDisruptor.getName(), centerLineDisruptor);
+    autoChooser.addOption(source3.getName(), source3);
+    autoChooser.addOption(amp3.getName(), amp3);
+    autoChooser.setDefaultOption(fourNoteAuto.getName(), fourNoteAuto);
+    SmartDashboard.putData(autoChooser);
+    SmartDashboard.putData("SetAuto", new InstantCommand(() -> {
+      autoCommand = autoChooser.getSelected();
+      SmartDashboard.putString("Selected Auto:", autoCommand.getName());
+    }).ignoringDisable(true));
+
+    debugMode.setDefaultOption("None (For Competition)", 0);
+    debugMode.addOption("Arm", ArmConstants.DEBUG_FLAG);
+    debugMode.addOption("Shooter", ShooterConstants.DEBUG_FLAG);
+    debugMode.addOption("Intake", IntakeConstants.DEBUG_FLAG);
+    debugMode.addOption("Note Pathway", ArmConstants.DEBUG_FLAG | ShooterConstants.DEBUG_FLAG | IntakeConstants.DEBUG_FLAG);
+    debugMode.addOption("StateMachine", CommandDebugFlags.NOTE_HANDLER);
+    debugMode.addOption("NotePath & StateMachine", 
+      ArmConstants.DEBUG_FLAG | ShooterConstants.DEBUG_FLAG | IntakeConstants.DEBUG_FLAG | CommandDebugFlags.NOTE_HANDLER);
+    debugMode.addOption("Swerve", Drivebase.DEBUG_FLAG);
+    debugMode.addOption("Vision", Vision.DEBUG_FLAG);
+    debugMode.addOption("AbsoluteDrive", CommandDebugFlags.ABS_DRIVE);
+    debugMode.addOption("AutoDrives", CommandDebugFlags.AUTO_SHOOT | CommandDebugFlags.ALIGN_TO_POSE | CommandDebugFlags.AUTO_AMP);
+    debugMode.addOption("AllDrive",
+      CommandDebugFlags.AUTO_SHOOT | CommandDebugFlags.ALIGN_TO_POSE | CommandDebugFlags.AUTO_AMP |
+      CommandDebugFlags.ABS_DRIVE | Drivebase.DEBUG_FLAG);
+    debugMode.addOption("Drive & Vision",
+      CommandDebugFlags.AUTO_SHOOT | CommandDebugFlags.ALIGN_TO_POSE | CommandDebugFlags.AUTO_AMP |
+      CommandDebugFlags.ABS_DRIVE | Drivebase.DEBUG_FLAG | Vision.DEBUG_FLAG);
+    debugMode.addOption("Climber", ClimberConstants.DEBUG_FLAG);
+    debugMode.addOption("ALL", ~0);
+    SmartDashboard.putData(debugMode);
+    SmartDashboard.putNumber(CommandDebugFlags.FLAGS_KEY, 0);
+    SmartDashboard.putData("Set Debug Mode", new InstantCommand(() -> 
+      SmartDashboard.putNumber(CommandDebugFlags.FLAGS_KEY, debugMode.getSelected())).ignoringDisable(true)
+    );
+
+    SmartDashboard.putData("setGains", new InstantCommand(drivebase::setVelocityModuleGains));
+    SmartDashboard.putData("SendAlliance",
+      new InstantCommand(
+        () -> drivebase.setAlliance(DriverStation.getAlliance().get())
+      )
+      .ignoringDisable(true)
+    );
+
+    SmartDashboard.putNumber("KV", Drivebase.KV);
+    SmartDashboard.putNumber("KA", Drivebase.KA);
+    SmartDashboard.putNumber("KP", 0);
+    SmartDashboard.putNumber("KI", 0);
+    SmartDashboard.putNumber("KD", 0);
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    /*
+     * operatorController.y().whileTrue(shooter.sysIdQuasiShoulder(SysIdRoutine.
+     * Direction.kForward));
+     * operatorController.b().whileTrue(shooter.sysIdQuasiShoulder(SysIdRoutine.
+     * Direction.kReverse));
+     * operatorController.a().whileTrue(shooter.sysIdDynShoulder(SysIdRoutine.
+     * Direction.kForward));
+     * operatorController.x().whileTrue(shooter.sysIdDynShoulder(SysIdRoutine.
+     * Direction.kReverse));
+     */
+    driveController.button(1).whileTrue(new AutoShoot(drivebase));
+    driveController.button(8).onTrue(new InstantCommand(drivebase::clearOdometrySeed).ignoringDisable(true));
+    operatorController.start().onTrue(new InstantCommand(drivebase::clearOdometrySeed).ignoringDisable(true));
+    /*driveController.button(2).whileTrue(new AutoAmp(drivebase)).onFalse(new InstantCommand(() -> {
+      SmartDashboard.putBoolean(Auton.AUTO_AMP_SCORE_KEY, false);
+      SmartDashboard.putBoolean(Auton.AUTO_AMP_ALIGN_KEY, false);
+    }));*/
+    headingController.button(1).onTrue(new InstantCommand(() -> SmartDashboard.putNumber(Auton.AUTO_INTAKE_SPEED_KEY, 1)))
+    .onFalse(new InstantCommand(() -> SmartDashboard.putNumber(Auton.AUTO_INTAKE_SPEED_KEY, 0)));
+    operatorController.povDown().onTrue(climber.runWinches(ClimberConstants.WINCH_DOWN_SPEED));
+    operatorController.povUp().onTrue(climber.runWinches(ClimberConstants.WINCH_UP_SPEED));
+    operatorController.povCenter().onTrue(climber.runWinches(0));
+    operatorController.back().onTrue(new InstantCommand(shooter::zeroEncoder).ignoringDisable(true));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return autoCommand;
+  }
+
+  public void stopDrive() {
+    drivebase.setChassisSpeeds(new ChassisSpeeds());
+  }
+
+  public void sendAlliance() {
+    drivebase.setAlliance(DriverStation.getAlliance().get());
+  }
+
+  public void setIsAuto(boolean isAuto) {
+    drivebase.isAuto = isAuto;
+  }
+
+  private Map<String, ChoreoTrajectory> loadTrajectories() {
+    Set<String> trajNames;
+    try {
+      if (Robot.isReal()) {
+        trajNames = listFilesUsingFilesList("/home/lvuser/deploy/choreo");
+      } else {
+        trajNames = listFilesUsingFilesList("src/main/deploy/choreo");
+      }
+    } catch (IOException e) {
+      DriverStation.reportError("Invalid Directory! Trajectories failed to load!", true);
+      return null;
+    }
+    return trajNames.stream().collect(Collectors.toMap(
+        entry -> entry.replace(".traj", ""),
+        entry -> Choreo.getTrajectory(entry.replace(".traj", ""))));
+  }
+
+  private Set<String> listFilesUsingFilesList(String dir) throws IOException {
+    try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+      return stream
+          .filter(file -> !Files.isDirectory(file))
+          .map(Path::getFileName)
+          .map(Path::toString)
+          .collect(Collectors.toSet());
+    }
+  }
+}
---
+++ Team 95 Code 2025/src/main/java/frc/robot/commands/AutoShoot.java
@@ -0,0 +1,182 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.Auton;
+import frc.robot.Constants.CommandDebugFlags;
+import frc.robot.Constants.Drivebase;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.Constants.Vision;
+import frc.robot.subsystems.SwerveBase;
+
+/** An example command that uses an example subsystem. */
+public class AutoShoot extends Command {
+  private final PIDController thetaController;
+  private final SwerveBase swerve;
+  private final double timeout;
+  private final Timer timer;
+
+  private double angle, omega;
+  private Pose2d currentPose;
+  private Translation3d speakerLocation;
+  private int debugFlags;
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public AutoShoot(SwerveBase swerve) {
+    this(swerve, -1);
+  }
+  public AutoShoot(SwerveBase swerve, double timeoutSeconds) {
+    thetaController = new PIDController(
+      Drivebase.HEADING_KP,
+      Drivebase.HEADING_KI,
+      Drivebase.HEADING_KD);
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    thetaController.setTolerance(Drivebase.HEADING_TOLERANCE);
+    this.swerve = swerve;
+    timeout = timeoutSeconds;
+    timer = new Timer();
+
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(swerve);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    SmartDashboard.putBoolean(Auton.ON_TARGET_KEY, false);
+
+    debugFlags = (int) SmartDashboard.getNumber(CommandDebugFlags.FLAGS_KEY, 0);
+
+    if (timeout >= 0) {
+      timer.reset();
+      timer.start();
+    }
+
+    var alliance = swerve.getAlliance();
+    if (alliance == Alliance.Blue) {
+      speakerLocation = Vision.BLUE_SPEAKER_POS;
+    } else if (alliance == Alliance.Red) {
+      speakerLocation = Vision.RED_SPEAKER_POS;
+    } else {
+      DriverStation.reportError("AutoShoot aborted because no alliance is set!", false);
+      cancel();
+    }
+
+    currentPose = swerve.getPose();
+
+    var centerPosDelta = speakerLocation.minus(new Translation3d(currentPose.getX(), currentPose.getY(), speakerLocation.getZ()));
+
+    // Calculate azimuth using position of center of robot:
+    // Azimuth:
+    // Note that we shoot from the back of the robot, so it may seem like this is backwards what it should be.
+    angle = new Rotation2d(centerPosDelta.getX(), centerPosDelta.getY())
+      .plus(Auton.AUTO_SHOOT_AZIMUTH_ADJUSTMENT)
+      .plus(Rotation2d.fromDegrees(180)).getRadians();
+
+    var shoulderLocation = 
+      // package current pose into translation3d
+      new Translation3d(currentPose.getX(), currentPose.getY(), 0)
+      // add mount location of arm, after converting to field-relative coordinates
+      .plus(
+        ArmConstants.PIVOT_LOCATION.rotateBy(
+          new Rotation3d(0, 0, -angle)
+        )
+      );  
+    var posDelta = speakerLocation.minus(shoulderLocation);
+
+    // Elevation (now with gravity!)
+    var range = posDelta.toTranslation2d().getNorm();
+    var height = posDelta.getZ();
+
+    if (range > ShooterConstants.AUTO_SHOOT_MAX_RANGE) {
+      DriverStation.reportError("AutoShoot aborted! Outside maximum range!", false);
+      cancel();
+    }
+
+    // Linearly scale speed to account for drag— we only care about average speed (drop => time-of-flight => avg. speed)
+    // so a linear interpolation is fine.
+    var speed = range * ShooterConstants.DRAG_INTERPOLATION_SLOPE + ShooterConstants.DRAG_INTERPOLATION_INTERCEPT;
+
+    // Coefficients for quadratic to do physics stuff
+    var A = -(Constants.GRAVITY * Math.pow(range, 2)) / (2 * Math.pow(speed, 2));
+    var B = range;
+    var C = A - height;
+    // Quadratic formula; simulating in desmos shows the addition root is the one we want
+    var root = (-B + Math.sqrt(Math.pow(B, 2) - 4 * A * C)) / (2 * A);
+    // Solving the quadratic gives the tangent of the angle
+    double elevation = Math.atan(root);
+
+    if (Double.isNaN(elevation) || (elevation > (Math.PI) / 2)) {
+      DriverStation.reportError("AutoShoot aborted!  Invalid solution!", false);
+      cancel();
+    }
+    if (elevation < ArmConstants.LOWER_LIMIT.getRadians()) {
+      elevation = ArmConstants.LOWER_LIMIT.getRadians() + ShooterConstants.DEAD_ZONE_FUDGE_OFFSET.getRadians();
+    }
+
+    SmartDashboard.putNumber(Auton.ARM_ANGLE_KEY, elevation);
+
+    thetaController.reset();
+
+    SmartDashboard.putBoolean(Auton.AUTO_SHOOTING_KEY, true);
+
+    if ((debugFlags & CommandDebugFlags.AUTO_SHOOT) != 0) {
+      SmartDashboard.putNumber("AutoShootHeading", angle);
+      SmartDashboard.putNumber("AutoShootRange", range);
+    }
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    debugFlags = (int) SmartDashboard.getNumber(CommandDebugFlags.FLAGS_KEY, 0);
+
+    omega = thetaController.calculate(swerve.getPose().getRotation().getRadians(), angle);
+    omega = (Math.abs(omega) < Drivebase.HEADING_MIN_ANGULAR_CONTROL_EFFORT) ? 0 : omega;
+    swerve.drive(new Translation2d(), omega, true, false);
+
+    if (thetaController.atSetpoint()) {
+      SmartDashboard.putBoolean(Auton.ON_TARGET_KEY, true);
+      swerve.setDriveBrake();
+    }
+
+    if ((debugFlags & CommandDebugFlags.AUTO_SHOOT) != 0) {
+      // Put SmartDashboard debug here
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    SmartDashboard.putBoolean(Auton.AUTO_SHOOTING_KEY, false);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if (timeout < 0) {
+      return false;
+    } else {
+      return timer.get() >= timeout;
+    }
+  }
+}
