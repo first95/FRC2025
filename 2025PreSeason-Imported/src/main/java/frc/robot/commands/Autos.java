6fadca1288715097f758285cd70bad87a51fef84
TomasKennelly <tomaskennelly7@gmail.com>
2025-02-01 11:04:07 -0500

    Attempting to make new folder in repo

---
+++ 2025PreSeason-Imported/src/main/java/frc/robot/commands/Autos.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Constants.Auton;
+import frc.robot.commands.autocommands.AlignToPose;
+import frc.robot.commands.autocommands.FollowTrajectory;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.SwerveBase;
+
+import java.sql.Driver;
+import java.util.Map;
+
+import choreo.trajectory.Trajectory;
+import choreo.auto.AutoFactory;
+import choreo.trajectory.SwerveSample;
+
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public final class Autos {
+  /** Example static factory for an autonomous command. */
+  private final AutoFactory autoFactory;
+  public static Command exampleAuto(ExampleSubsystem subsystem) {
+    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+  }
+
+ 
+
+  private Autos(SwerveBase swerve) {
+    autoFactory = new AutoFactory(
+      swerve::getPose,
+      swerve::resetOdometry,
+      swerve::followTrajectory,
+      true,
+      swerve);
+  }
+}
